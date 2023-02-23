a3f3f063872d660433ebb053b42af5a4997246ab
Алекс <ama@mail.ru>
2023-02-23 11:19:43 +0300

    Начальный

---
+++ classes.py
@@ -0,0 +1,15 @@
+class A:
+    def __init__(self, value_: float, color_: str):
+        self.value = value_
+        self.color = color_
+
+
+class B:
+    def __init__(self, function_: str, value_: str):
+        self.function = function_
+        self.value = value_
+
+
+class C:
+    def __init__(self, value_: list):
+        self.value = value_
