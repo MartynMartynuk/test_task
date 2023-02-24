a56b13f0d04110a576388d873ab23ba3a90cc324
Алекс <ama@mail.ru>
2023-02-24 12:34:59 +0300

    Закончил function_2

--- handlers.py
+++ handlers.py
@@ -2,43 +2,54 @@ import json
 from classes import *
 from maps import *
 
-# a = '''[
-#     {"value": 1, "color": "red"},
-#     {"value": 2, "color": "green"},
-#     {"value": -2, "color": "blue"}
-# ]'''
-# b = '''[
-#     {"function": "prod", "value": 2},
-#     {"function": "sum", "value": 1},
-#     {"function": "pow", "value": -2}
-# ]'''
+a = '''[
+    {"value": 1, "color": "red"},
+    {"value": 2, "color": "green"},
+    {"value": -2, "color": "blue"}
+]'''
+b = '''[
+    {"function": "prod", "value": 2},
+    {"function": "sum", "value": 1},
+    {"function": "pow", "value": -2}
+]'''
 
-# c = [
-#     {"value": [2, 0, 0]},
-#     {"value": [0, 3, 0]},
-#     {"value": [0, 0, -0.25]}
-# ]
+c = [
+    {"value": [2, 0, 0]},
+    {"value": [0, 3, 0]},
+    {"value": [0, 0, -0.25]}
+]
 
-a = '''{"value": 1, "color": "red"}'''
-b = '''{"function": "prod", "value": 2}'''
-c = '''{"value": [2, 0, 0]}'''
+
+# a = '''{"value": 1, "color": "red"}'''
+# b = '''{"function": "prod", "value": 2}'''
+# c = '''{"value": [2, 0, 0]}'''
 
 
 def function_2(a_json, b_json):
     a = json.loads(a_json)
     b = json.loads(b_json)
     if type(a) is list and type(b) is list:
-        pass
+        c_lst = []
+        i = 0
+        iter_a = iter(a)
+        iter_b = iter(b)
+
+        while i < len(a):
+            c_lst.append(single_mean_handler(next(iter_a), next(iter_b)))
+            i += 1
+        return json.dumps(c_lst)
     elif type(a) is dict and type(b) is dict:
-        return handler(a, b)
+        return single_mean_handler(a, b)
 
 
-def handler(a, b):
+def single_mean_handler(a, b):
     a_obj = A(a['value'], a['color'])
     b_obj = B(b['function'], b['value'])
     res = function_mapper(b_obj.function, a_obj.value, b_obj.value)
     lst = [0] * 3
-    lst[color_mapper[a_obj.color]: int] = res
+    color_index = color_mapper[a_obj.color]
+
+    lst[color_index] = res
     c_obj = C(lst)
     return c_obj.toJSON()
 
