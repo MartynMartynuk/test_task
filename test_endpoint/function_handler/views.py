d67958093d886de1e081033b63b8cc036f31f031
Алекс <ama@mail.ru>
2023-02-25 20:42:15 +0300

    Добавил api эндпоинты 2

--- test_endpoint/function_handler/views.py
+++ test_endpoint/function_handler/views.py
@@ -1,12 +1,11 @@
 from django.http import JsonResponse
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 
 
 @api_view(['POST'])
 def api_function_2(request):
     if request.method == 'POST':
-        return JsonResponse({'function 2': 'alive!'}, safe=False)
+        return JsonResponse(request.data, safe=False)
 
 
 @api_view(['POST'])
