b51b0d37518b7f8d98d7dd18f3dc52e55ea5b70e
Алекс <ama@mail.ru>
2023-02-25 19:26:51 +0300

    Создал модели

--- test_endpoint/function_handler/models.py
+++ test_endpoint/function_handler/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class A(models.Model):
+    value = models.FloatField()
+    color = models.CharField()
+
+
+class B(models.Model):
+    function = models.CharField()
+    value = models.FloatField()
+
+
+class C(models.Model):
+    value = models.JSONField()
