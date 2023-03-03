93bf6090864ff949568013dc5e2dd4201e042e23
ama@mail.ru <ama>
2023-03-03 09:40:09 +0400

    Добавил тест множественного ввода

--- test.py
+++ test.py
@@ -1,21 +1,55 @@
 import requests
 
 # Call 2
-# r = requests.post("http://127.0.0.1:8008/function_2/", 
-#                   json=[{"value": 2, "color": "green"}, 
-#                         {"function": "sum", "value": 3}])
-# print(r.content, r.status_code, r.reason)
+r = requests.post("http://127.0.0.1:8008/function_2/",
+                  json=[{"value": 2, "color": "green"},
+                        {"function": "sum", "value": 3}])
+print(r.content, r.status_code, r.reason)
+
+# Call 2 many
+r = requests.post("http://127.0.0.1:8008/function_2/many/",
+                  json=[
+                      [
+                          {
+                              "value": 1,
+                              "color": "red"
+                          },
+                          {
+                              "value": 2,
+                              "color": "green"
+                          },
+                          {
+                              "value": -2,
+                              "color": "blue"
+                          }
+                      ],
+                      [
+                          {
+                              "function": "prod",
+                              "value": 2
+                          },
+                          {
+                              "function": "sum",
+                              "value": 1
+                          },
+                          {
+                              "function": "pow",
+                              "value": -2
+                          }
+                      ]
+                  ])
+print(r.content, r.status_code, r.reason)
 
 # Add A
-# r = requests.post("http://127.0.0.1:8008/add/", 
-#                   json={"value": 2, "color": "green"})
-# print(r.content, r.status_code, r.reason)
+r = requests.post("http://127.0.0.1:8008/add/",
+                  json={"value": 2, "color": "green"})
+print(r.content, r.status_code, r.reason)
 
 # Add B
-# r = requests.post("http://127.0.0.1:8008/add/", 
-#                   json={"function": "sum", "value": 3})
-# print(r.content, r.status_code, r.reason)
+r = requests.post("http://127.0.0.1:8008/add/",
+                  json={"function": "sum", "value": 3})
+print(r.content, r.status_code, r.reason)
 
 # Call 3
-# r = requests.post("http://127.0.0.1:8008/function_3/1/1/")
-# print(r.content, r.status_code, r.reason)
+r = requests.post("http://127.0.0.1:8008/function_3/1/1/")
+print(r.content, r.status_code, r.reason)
